4dcc5f643aac88023beb7a8e9861ccda5cb9ec08
David Gaspar <davsangas@hotmail.com>
2023-08-02 20:48:59 -0600

    Feat: Scroll by navbar

--- src/Components/Content.jsx
+++ src/Components/Content.jsx
@@ -2,10 +2,17 @@ import { BsGithub } from "react-icons/bs";
 import { BiRightArrowAlt } from "react-icons/bi";
 
 const Content = () => {
+    const projectsData = [
+        {},
+        {},
+        {},
+        {},
+        {},
+    ]
     return (
         <div>
             <div className="w-full h-full flex flex-col mt-6">
-                <section className="info-container mt-40 pl-5 mb-56 sm:pl-32 sm:pr-32 sm:mb-56 md:pl-56 md:pr-56">
+                <section id="Home" className="Home info-container mt-40 pl-5 mb-56 sm:pl-32 sm:pr-32 sm:mb-56 md:pl-56 md:pr-56">
                     <h1 className="text-white text-xl sm:text-2xl">Hi! I'm David 👋</h1>
                     <p className="flex mt-2 bg-gradient-to-r  from-cyan-200 to-cyan-400 text-transparent text-5xl sm:text-7xl font-bold bg-clip-text dark:bg-gradient-to-r dark:from-gray-200 dark:via-gray-400 dark:to-gray-600">
                         Frontend Developer
@@ -13,7 +20,7 @@ const Content = () => {
                     </p>
                 </section>
                 <div className="border w-full dark:border-gray-400" />
-                <section className="pl-5 pt-10 sm:pl-32 sm:pr-32 md:pl-56 md:pr-56">
+                <section id="About" className="About-section pl-5 pt-10 sm:pl-32 sm:pr-32 md:pl-56 md:pr-56">
                     <h1 className="text-5xl sm:text-7xl font-bold text-cyan-400 dark:text-cyan-200">
                         About me
                         <span className="text-cyan-600">.</span>
@@ -68,7 +75,7 @@ const Content = () => {
                     </div>
                 </section>
                 <div className="border w-full dark:border-gray-400" />
-                <section className="p-5 pt-10 pb-10 sm:pl-32 sm:pr-32 md:pl-56 md:pr-56">
+                <section id="Projects" className="Project-Section p-5 pt-10 pb-10 sm:pl-32 sm:pr-32 md:pl-56 md:pr-56">
                     <p className="flex mt-2 text-5xl sm:text-7xl font-bold text-cyan-400 dark:text-cyan-200">
                         Projects
                         <span className="text-cyan-600">.</span>
@@ -82,10 +89,10 @@ const Content = () => {
                                 <p className="mb-2">A translator build with React,Javascript and Chat GPT API </p>
                                 <div className="grid grid-cols-2 gap-4 dark:text-gray-200">
                                     <div className="border-2 px-2 py-1 rounded-lg cursor-pointer hover:scale-105 transition-all hover:bg-blue-300 dark:hover:bg-cyan-800 dark:border-gray-400">
-                                        <a className="flex items-center justify-between">Code <span><BsGithub /></span></a>
+                                        <a className="flex items-center justify-between" href="https://github.com/DavidS2602/Translator.git">Code <span><BsGithub /></span></a>
                                     </div>
                                     <div className="border-2 px-2 py-1 rounded-lg cursor-pointer hover:scale-105 transition-all bg-gray-200 hover:bg-gray-300  dark:hover:bg-cyan-800 dark:border-gray-400 dark:bg-gray-700">
-                                        <a className="flex items-center gap-2" href="">Go to page <span><BiRightArrowAlt /></span></a>
+                                        <a className="flex items-center gap-2" href="https://startling-sunflower-1fd25f.netlify.app/">Go to page <span><BiRightArrowAlt /></span></a>
                                     </div>
                                 </div>
                             </div>
@@ -98,14 +105,13 @@ const Content = () => {
                                 <p className="mb-2">A translator build with React,Javascript and Chat GPT API </p>
                                 <div className="grid grid-cols-2 gap-4 dark:text-gray-200">
                                     <div className="border-2 px-2 py-1 rounded-lg cursor-pointer hover:scale-105 transition-all hover:bg-blue-300 dark:hover:bg-cyan-800 dark:border-gray-400">
-                                        <a className="flex items-center justify-between">Code <span><BsGithub /></span></a>
+                                        <a className="flex items-center justify-between" href="https://github.com/DavidS2602/clon-tesla-landing.git">Code <span><BsGithub /></span></a>
                                     </div>
                                     <div className="border-2 px-2 py-1 rounded-lg cursor-pointer hover:scale-105 transition-all bg-gray-200 hover:bg-gray-300  dark:hover:bg-cyan-800 dark:border-gray-400 dark:bg-gray-700">
-                                        <a className="flex items-center gap-2" href="">Go to page <span><BiRightArrowAlt /></span></a>
+                                        <a className="flex items-center gap-2" href="https://jocular-zabaione-29d155.netlify.app/">Go to page <span><BiRightArrowAlt /></span></a>
                                     </div>
                                 </div>
                             </div>
-
                         </div>
                         <div className="border-2 rounded-lg inline-block">
                             <div className="image-section">
@@ -115,10 +121,10 @@ const Content = () => {
                                 <p className="mb-2">A translator build with React,Javascript and Chat GPT API </p>
                                 <div className="grid grid-cols-2 gap-4 dark:text-gray-200">
                                     <div className="border-2 px-2 py-1 rounded-lg cursor-pointer hover:scale-105 transition-all hover:bg-blue-300 dark:hover:bg-cyan-800 dark:border-gray-400">
-                                        <a className="flex items-center justify-between">Code <span><BsGithub /></span></a>
+                                        <a className="flex items-center justify-between" href="https://github.com/DavidS2602/clon-la-velada.git">Code <span><BsGithub /></span></a>
                                     </div>
                                     <div className="border-2 px-2 py-1 rounded-lg cursor-pointer hover:scale-105 transition-all bg-gray-200 hover:bg-gray-300  dark:hover:bg-cyan-800 dark:border-gray-400 dark:bg-gray-700">
-                                        <a className="flex items-center gap-2" href="">Go to page <span><BiRightArrowAlt /></span></a>
+                                        <a className="flex items-center gap-2" href="https://visionary-moonbeam-c97fbd.netlify.app/">Go to page <span><BiRightArrowAlt /></span></a>
                                     </div>
                                 </div>
                             </div>
@@ -132,10 +138,10 @@ const Content = () => {
                                 <p className="mb-2">A translator build with React,Javascript and Chat GPT API </p>
                                 <div className="grid grid-cols-2 gap-4 dark:text-gray-200">
                                     <div className="border-2 px-2 py-1 rounded-lg cursor-pointer hover:scale-105 transition-all hover:bg-blue-300 dark:hover:bg-cyan-800 dark:border-gray-400">
-                                        <a className="flex items-center justify-between">Code <span><BsGithub /></span></a>
+                                        <a className="flex items-center justify-between" href="https://github.com/DavidS2602/Play-with-React.git">Code <span><BsGithub /></span></a>
                                     </div>
                                     <div className="border-2 px-2 py-1 rounded-lg cursor-pointer hover:scale-105 transition-all bg-gray-200 hover:bg-gray-300  dark:hover:bg-cyan-800 dark:border-gray-400 dark:bg-gray-700">
-                                        <a className="flex items-center gap-2" href="">Go to page <span><BiRightArrowAlt /></span></a>
+                                        <a className="flex items-center gap-2" href="https://aquamarine-torrone-bff9bc.netlify.app/">Go to page <span><BiRightArrowAlt /></span></a>
                                     </div>
                                 </div>
                             </div>
@@ -149,10 +155,10 @@ const Content = () => {
                                 <p className="pb-2">A translator build with React,Javascript and Chat GPT API </p>
                                 <div className="grid grid-cols-2 gap-4 dark:text-gray-200">
                                     <div className="border-2 px-2 py-1 rounded-lg cursor-pointer hover:scale-105 transition-all hover:bg-blue-300 dark:hover:bg-cyan-800 dark:border-gray-400">
-                                        <a className="flex items-center justify-between">Code <span><BsGithub /></span></a>
+                                        <a className="flex items-center justify-between" href="https://github.com/DavidS2602/Todo-App.git">Code <span><BsGithub /></span></a>
                                     </div>
                                     <div className="border-2 px-2 py-1 rounded-lg cursor-pointer hover:scale-105 transition-all bg-gray-200 hover:bg-gray-300  dark:hover:bg-cyan-800 dark:border-gray-400 dark:bg-gray-700">
-                                        <a className="flex items-center gap-2" href="">Go to page <span><BiRightArrowAlt /></span></a>
+                                        <a className="flex items-center gap-2" href="https://shimmering-pastelito-2d1903.netlify.app/">Go to page <span><BiRightArrowAlt /></span></a>
                                     </div>
                                 </div>
                             </div>
--- src/Components/Navbar.jsx
+++ src/Components/Navbar.jsx
@@ -2,17 +2,24 @@ import SunIcon from "../assets/SunIcon";
 import MoonIcon from './../assets/MoonIcon';
 
 const Navbar = ({ darkMode, onClick }) => {
+    const handleNavigation = (sectionId) => {
+        const section = document.getElementById(sectionId);
+        if (section) {
+            section.scrollIntoView({ behavior: "smooth" });
+        }
+    }
+
     return (
         <nav className="flex w-full font-sans bg-sky-300 dark:bg-sky-700 fixed items-center justify-between px-4 py-4 border-b-2 dark:border-gray-400 transition-colors duration-600 dark:text-gray-300">
             <p className="font-bold text-3xl">CodeDS</p>
             <div className="flex gap-4 items-center font-semibold">
-                <div className="hover:text-blue-800 transition-transform duration-300 dark:hover:text-cyan-500 hover:scale-110 cursor-pointer">
+                <div onClick={() => handleNavigation('Home')} className="hover:text-blue-800 transition-transform duration-300 dark:hover:text-cyan-500 hover:scale-110 cursor-pointer">
                     <p>Home</p>
                 </div>
-                <div className="hover:text-blue-800 transition-transform duration-300 dark:hover:text-cyan-500 hover:scale-110 cursor-pointer">
+                <div onClick={() => handleNavigation('About')} className="hover:text-blue-800 transition-transform duration-300 dark:hover:text-cyan-500 hover:scale-110 cursor-pointer">
                     <p>About</p>
                 </div>
-                <div className="hover:text-blue-800 transition-transform duration-300 dark:hover:text-cyan-500 hover:scale-110 cursor-pointer">
+                <div onClick={() => handleNavigation('Projects')} className="hover:text-blue-800 transition-transform duration-300 dark:hover:text-cyan-500 hover:scale-110 cursor-pointer">
                     <p>Projects</p>
                 </div>
                 <div className="hover:text-blue-800 transition-transform duration-300 dark:hover:text-cyan-500 hover:scale-110 cursor-pointer">
