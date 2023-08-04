aa949d8521a3779a56df6b21d606180ab2f45912
David Gaspar <davsangas@hotmail.com>
2023-08-03 18:16:01 -0600

    Feat: Navbar responsive and sidebar

--- src/Components/Navbar.jsx
+++ src/Components/Navbar.jsx
@@ -1,35 +1,53 @@
+import { useState } from "react";
 import SunIcon from "../assets/SunIcon";
 import MoonIcon from './../assets/MoonIcon';
+import { AiOutlineMenu } from 'react-icons/ai';
+import Sidebar from "./Sidebar";
+
 
 const Navbar = ({ darkMode, onClick }) => {
+    const [SidebarOpen, setSidebarOpen] = useState('false')
+
     const handleNavigation = (sectionId) => {
         const section = document.getElementById(sectionId);
         if (section) {
             section.scrollIntoView({ behavior: "smooth" });
+            setSidebarOpen(false)
         }
     }
 
+    const handleSidebar = () => {
+        setSidebarOpen(!SidebarOpen)
+    }
+
     return (
-        <nav className="flex w-full font-sans bg-sky-300 dark:bg-sky-700 fixed items-center justify-between px-4 py-4 border-b-2 dark:border-gray-400 transition-colors duration-600 dark:text-gray-300">
-            <p className="font-bold text-3xl">CodeDS</p>
-            <div className="flex gap-4 items-center font-semibold">
-                <div onClick={() => handleNavigation('root')} className="hover:text-blue-800 transition-transform duration-300 dark:hover:text-cyan-500 hover:scale-110 cursor-pointer">
-                    <p>Home</p>
+        <div>
+
+            <nav className="flex w-full font-sans bg-sky-300 dark:bg-sky-700 fixed items-center justify-between px-4 py-4 border-b-2 dark:border-gray-400 transition-colors duration-600 dark:text-gray-300">
+                <p className="font-bold text-3xl">CodeDS</p>
+                <div className="flex gap-4 items-center font-semibold">
+                    <div onClick={() => handleNavigation('root')} className="hover:text-blue-800 transition-transform duration-300 hidden sm:block dark:hover:text-cyan-500 hover:scale-110 cursor-pointer">
+                        <p>Home</p>
+                    </div>
+                    <div onClick={() => handleNavigation('About')} className="hover:text-blue-800 transition-transform duration-300 hidden sm:block dark:hover:text-cyan-500 hover:scale-110 cursor-pointer">
+                        <p>About</p>
+                    </div>
+                    <div onClick={() => handleNavigation('Projects')} className="hover:text-blue-800 transition-transform duration-300 hidden sm:block dark:hover:text-cyan-500 hover:scale-110 cursor-pointer">
+                        <p>Projects</p>
+                    </div>
+                    <div className="hover:text-blue-800 transition-transform duration-300 hidden sm:block dark:hover:text-cyan-500 hover:scale-110 cursor-pointer">
+                        <p>Contact</p>
+                    </div>
+                    <div className="sm:hidden flex items-center">
+                        <button onClick={handleSidebar}><AiOutlineMenu size={23} /></button>
+                    </div>
+                    <div className="rounded-full flex items-center p-1">
+                        <button onClick={onClick}>{darkMode ? <SunIcon /> : <MoonIcon />}</button>
+                    </div>
                 </div>
-                <div onClick={() => handleNavigation('About')} className="hover:text-blue-800 transition-transform duration-300 dark:hover:text-cyan-500 hover:scale-110 cursor-pointer">
-                    <p>About</p>
-                </div>
-                <div onClick={() => handleNavigation('Projects')} className="hover:text-blue-800 transition-transform duration-300 dark:hover:text-cyan-500 hover:scale-110 cursor-pointer">
-                    <p>Projects</p>
-                </div>
-                <div className="hover:text-blue-800 transition-transform duration-300 dark:hover:text-cyan-500 hover:scale-110 cursor-pointer">
-                    <p>Contact</p>
-                </div>
-                <div className="rounded-full flex items-center p-1">
-                    <button onClick={onClick}>{darkMode ? <SunIcon /> : <MoonIcon />}</button>
-                </div>
-            </div>
-        </nav>
+            </nav>
+            {SidebarOpen && <Sidebar handleNavigation={handleNavigation} setSidebar={setSidebarOpen}/>}
+        </div>
     );
 }
 
---
+++ src/Components/Sidebar.jsx
@@ -0,0 +1,26 @@
+import { AiOutlineClose } from 'react-icons/ai'
+const Sidebar = ({ handleNavigation, setSidebar }) => {
+    const handleClose = () => {
+        setSidebar(false)
+    }
+    return (
+        <nav className="fixed right-0 top-[69px] rounded-bl-lg w-60 bg-sky-300 dark:bg-sky-700 p-4 border-l-2 border-b-2 dark:border-gray-400 transition-colors duration-600 dark:text-gray-300">
+            <div className='cursor-pointer'>
+                <AiOutlineClose onClick={handleClose} size={23} className='hover:text-cyan-700 hover:scale-105 duration-300'/>
+            </div>
+            <div className="flex flex-col gap-4 items-center font-semibold">
+                <div onClick={() => handleNavigation('root')} className="cursor-pointer hover:text-blue-800 dark:hover:text-cyan-500 hover:scale-105 duration-300">
+                    <p>Home</p>
+                </div>
+                <div onClick={() => handleNavigation('About')} className="cursor-pointer hover:text-blue-800 dark:hover:text-cyan-500 hover:scale-105 duration-300">
+                    <p>About</p>
+                </div>
+                <div onClick={() => handleNavigation('Projects')} className="cursor-pointer hover:text-blue-800 dark:hover:text-cyan-500 hover:scale-105 duration-300">
+                    <p>Projects</p>
+                </div>
+            </div>
+        </nav>
+    );
+}
+
+export default Sidebar;
